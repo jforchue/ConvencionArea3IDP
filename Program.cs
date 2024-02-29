f96c544474a774a160a2f0282ad9d6d89b9a414f
Junior Betances <jbetances@uce.edu.do>
2024-02-29 12:45:30 -0400

    .

--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
+
+
+DefaultFilesOptions defaultFilesOptions = new DefaultFilesOptions();
+defaultFilesOptions.DefaultFileNames.Clear();
+defaultFilesOptions.DefaultFileNames.Add("YoSoySalario.html");
+app.UseDefaultFiles(defaultFilesOptions);
+
+app.UseStaticFiles();
 
 app.Run();
